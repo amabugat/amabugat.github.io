fcab2e77aaed20861dfaa1e1fbbdd79f7a7ad4e3
amabugat <34631534+amabugat@users.noreply.github.com>
2017-12-18 22:17:22 -0800

    Still trying to fix scrolling
    Script.js is still throwing erros of undefined '$'

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-$(window).scroll(function() {
-    parallax();
+//$(window).scroll(function() {
+//    parallax();
+//})
+//
+//function parallax() {
+//    var wScroll = $(window).scrollTop();
+//    
+//    $('.parallax--bg').css('background-position', 'center ' + (wScroll * .2) + 'px ');
+//    console.log(wScroll);
+//}
+
+$(window).scroll(function(e) {
+  parallax();
 })
 
+
 function parallax() {
-    var wScroll = $(window).scrollTop();
+  var scroll = $(window).scrollTop();
+  var screenHeight = $(window).height();
+
+  $('.parallax--bg').each(function() {
+    var offset = $(this).offset().top;
+    var distanceFromBottom = offset - scroll - screenHeight
     
-    $('.parallax--bg').css('background-position', 'center ' + (wScroll * .2) + 'px ');
-    console.log(wScroll);
+    if (offset > screenHeight && offset) {
+      $(this).css('background-position', 'center ' + (( distanceFromBottom  ) * 0.5) +'px');
+    } else {
+      $(this).css('background-position', 'center ' + (( -scroll ) * 0.5) + 'px');
+    }
+  })
 }
